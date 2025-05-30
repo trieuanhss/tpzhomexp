dbeae3092159600efcdcd70d740532a5b06d4970
MrLong <trieuanhss@gmail.com>
2025-05-30 23:16:12 +0700

    update text

--- src/components/Windows/MyProjects/PangaiaContent.vue
+++ src/components/Windows/MyProjects/PangaiaContent.vue
@@ -1,6 +1,10 @@
 <template>
 	<div class="text-xs">
 		<p>TPZ-Tech Studio</p>
+		<p>Hình ảnh</p>
+		<p>Mô tả</p>
+		<p>Công nghệ sử dụng</p>
+		<p>Phản hồi người dùng</p>
 		<p>đang cập nhật...</p>
 	</div>
   </template>
